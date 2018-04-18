f161a9962eb55f736a714d790701bd2a20533e26
terri <thuynh1989@gmail.com>
2018-04-18 12:26:49 -0600

    bot now responds and presents options to users in its messages

--- src/handleActions.js
+++ src/handleActions.js
@@ -19,43 +19,51 @@ module.exports = (action) => {
             return "<script>sendMsg('start_climateQ1')</script>";
         case "categoryClimate.categoryClimate-no":
             return "<script>sendMsg('start_climateQ1')</script>";
+
+        /**CLIMATE QUESTION 1**/    
         case "climateQ1":
-            return "<p><form><input type='radio' name='climateQ1' value='yes' onchange='radioSelected(\"climateQ1\")'> Yes\
-                    <br><input type='radio' name='climateQ1' value='no' onchange='radioSelected(\"climateQ1\")'> No\
+            return "<p><form><input type='radio' name='climateQ1' value='climateQ1_yes' onchange='radioSelected(\"climateQ1\")'> Yes\
+                    <br><input type='radio' name='climateQ1' value='climateQ1_no' onchange='radioSelected(\"climateQ1\")'> No\
                     </form></p>";
         case "climateQ1.climateQ1-custom":
             // follow up response
             return "<script>sendMsg('start_climateQ2')</script>";
+
+        /**CLIMATE QUESTION 2**/    
         case "climateQ2":
             return "<p><form><input type='radio' name='climateQ2' value='yes' onchange='radioSelected(\"climateQ2\")'> Yes\
                     <br><input type='radio' name='climateQ2' value='no' onchange='radioSelected(\"climateQ2\")'> No\
                     </form></p>";
         case "climateQ2.climateQ2-yes":
-            return "<p><form><input type='radio' name='climateQ2_followup' value='less than 1cm' onchange='radioSelected(\"climateQ2_followup\")'> Less than 1 centimetre\
-                    <br><input type='radio' name='climateQ2_followup' value='between 1-4cm' onchange='radioSelected(\"climateQ2_followup\")'> Between 1 and 4 centimetres\
-                    <br><input type='radio' name='climateQ2_followup' value='more than 4cm' onchange='radioSelected(\"climateQ2_followup\")'> More than 4 centimetres\
+            return "<p><form><input type='radio' name='climateQ2_followup' value='less_than_1cm' onchange='radioSelected(\"climateQ2_followup\")'> Less than 1 centimetre\
+                    <br><input type='radio' name='climateQ2_followup' value='between_1-4cm' onchange='radioSelected(\"climateQ2_followup\")'> Between 1 and 4 centimetres\
+                    <br><input type='radio' name='climateQ2_followup' value='more_than_4cm' onchange='radioSelected(\"climateQ2_followup\")'> More than 4 centimetres\
                     </form></p>";
         case "climateQ2.climateQ2-no":
             return "<script>sendMsg('start_climateQ3')</script>";
         case "climateQ2.climateQ2-yes.climateQ2-yes-custom":
             return "<script>sendMsg('start_climateQ3')</script>";
+        
+        /**CLIMATE QUESTION 3**/    
         case "climateQ3":
             return "<p><form><input type='radio' name='climateQ3' value='yes' onchange='radioSelected(\"climateQ3\")'> Yes\
                     <br><input type='radio' name='climateQ3' value='no' onchange='radioSelected(\"climateQ3\")'> No\
                     </form></p>";
         case "climateQ3.climateQ3-yes":
-            return "<p><form><input type='radio' name='climateQ3_followup' value='birds' onchange='radioSelected(\"climateQ3_followup\")'> Birds\
-                    <br><input type='radio' name='climateQ3_followup' value='fish' onchange='radioSelected(\"climateQ3_followup\")'> Fish\
-                    <br><input type='radio' name='climateQ3_followup' value='polar bears' onchange='radioSelected(\"climateQ3_followup\")'> Polar Bears\
+            return "<p><form><input type='radio' name='climateQ3_followup' value='climateQ3_birds' onchange='radioSelected(\"climateQ3_followup\")'> Birds\
+                    <br><input type='radio' name='climateQ3_followup' value='climateQ3_fish' onchange='radioSelected(\"climateQ3_followup\")'> Fish\
+                    <br><input type='radio' name='climateQ3_followup' value='climateQ3_polar_bears' onchange='radioSelected(\"climateQ3_followup\")'> Polar Bears\
                     </form></p>";
         case "climateQ3.climateQ3-no":
             return "<script>sendMsg('start_climateQ4')</script>";
-        case "climateQ3.climateQ3-yes.climateQ2-yes-custom":
+        case "climateQ3.climateQ3-yes.climateQ3-yes-custom":
             return "<script>sendMsg('start_climateQ4')</script>";
+        
+        /**CLIMATE QUESTION 4**/    
         case "climateQ4":
-            return "<p><form><input type='radio' name='climateQ4' value='less than 0.05%' onchange='radioSelected(\"climateQ4\")'> Less than 0.05%\
-                    <br><input type='radio' name='climateQ4' value='between 0.06% and 1%' onchange='radioSelected(\"climateQ4\")'> Between 0.06% and 1%\
-                    <br><input type='radio' name='climateQ4' value='more than 1%' onchange='radioSelected(\"climateQ4\")'> More than 1%\
+            return "<p><form><input type='radio' name='climateQ4' value='less_than_0.05%' onchange='radioSelected(\"climateQ4\")'> Less than 0.05%\
+                    <br><input type='radio' name='climateQ4' value='between_0.06%_and_1%' onchange='radioSelected(\"climateQ4\")'> Between 0.06% and 1%\
+                    <br><input type='radio' name='climateQ4' value='more_than_1%' onchange='radioSelected(\"climateQ4\")'> More than 1%\
                     </form></p>";
         default:
             return "";
